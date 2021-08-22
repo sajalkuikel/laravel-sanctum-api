35ab3a66dcbde83d729797465603c22ceb00cabc
Sajal Kuikel <sajalkuikel.edu@gmail.com>
2021-08-22 22:36:24 +0545

    CRUD api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Product[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => ['required' , 'min:3' ],
+                'description' => ['required', 'min:10' , 'max:255' ],
+                'price' => ['required']
+            ]
+        );
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return Product::find($product);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        Product::where('id', $product->id )->update($request->all());
+
+        return ['message' => ' Product updated!'];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //Product::destroy($product->id);
+
+        //both are equivalent
+
+        $product->delete();
+
+        return ['message' => ' Product deleted!'];
+    }
+}
