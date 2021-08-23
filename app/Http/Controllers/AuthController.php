b3aa5729469a1a2f110771c5776ecfd94d790d8a
Sajal Kuikel <sajalkuikel.edu@gmail.com>
2021-08-23 13:35:31 +0545

    login/logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,9 +12,11 @@ class AuthController extends Controller
     public function register ( Request $request ){
         $request->validate([
             'name' => 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email',
             'password' => 'required|min:8'
         ]);
+
+        // Hashing the entered password with default hash
         $request['password'] = bcrypt($request['password']);
 
         $user = User::create($request->all());
@@ -22,5 +24,43 @@ class AuthController extends Controller
         $token = $user->createToken('token')->plainTextToken;
 
         return ['message' => 'Registered!' ,  'token' => $token ];
-     }
+    }
+
+
+    public function logout(){
+
+        //destroy all the tokens associated with the user.
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Logged out!'
+        ];
+    }
+
+
+    public function login ( Request $request ){
+
+//        validate the login request
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        //email check
+        $user  = User::where('email' , $request->email)->first();
+
+        //email and password check
+        if( !$user ||  !Hash::check($request->password , $user->password) ){
+            return [
+                "message" => 'Invalid credentials!'
+            ];
+        }
+
+        // Generate the token if the credentials are correct
+        $token = $user->createToken('token')->plainTextToken;
+
+
+        //Return a success message
+        return ['message' => 'Logged in !' ,  'token' => $token ];
+    }
+
 }
